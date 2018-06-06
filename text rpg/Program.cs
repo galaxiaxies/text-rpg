e2b9be5972edcc6314083fbbbaee6b0c079f3259
gabriel <osbrin@gmail.com>
2018-06-06 03:43:23 -0500

    First commit

---
+++ text rpg/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace text_rpg {
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Adventure!!");
+            Console.Read();
+        }
+    }
+}
