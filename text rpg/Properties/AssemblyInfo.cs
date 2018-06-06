5c9f4ee06d1cd089ef8304bf54da2e27c63e16b8
gabriel <osbrin@gmail.com>
2018-06-06 04:09:36 -0500

    GDD updated

--- text rpg/Properties/AssemblyInfo.cs
+++ text rpg/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("text_rpg")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("galaxiaxies")]
 [assembly: AssemblyProduct("text_rpg")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
